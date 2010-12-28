98c54ea1fb5c9a3b32072c5a253cb2aa94dfbcf6
Mark Lamourine <markllama@gmail.com>
2010-12-27 19:53:17 -0500

    added Point object so I can convert between that and Hex Vectors

--- python/src/hexmap/hexmapview.py
+++ python/src/hexmap/hexmapview.py
@@ -2,10 +2,14 @@
 Create a graphical version of the hex map
 """
 
+from Tkinter import *
+
+def sin60(a):
+    return (a * 8660) / 10000
+
 from hexmap import HexMap
 from vector import Vector
 
-from Tkinter import *
 
 class HexMapView(HexMap):
 
@@ -14,4 +18,34 @@ class HexMapView(HexMap):
 
 
         HexMap.__init__(self, size, origin, terrains, tokens, 
-                        name, game, copyright, hexruns)
+                        name, game, copyright)
+
+        # setting this syncs the others
+        self.hexrun = hexrun
+
+    # All of the hex dimensions are based off the hexrun
+    @property
+    def hexrun(self): return self._hexrun
+
+    @hexrun.setter
+    def hexrun(self, value):
+        self._hexrun = value
+        self._hexradius = value * 2
+        self._hexwidth = value * 4
+        self._hexrise = sin60(self._hexradius)
+        self._hexheight = self._hexrise * 2
+
+    @property
+    def hexradius(self): return self._hexradius
+        
+    @property
+    def hexrise(self): return self._hexrise
+
+    @property
+    def hexheight(self): return self._hexheight
+
+    @property
+    def canvasSize(self):
+        x = (self.hexrun * 3 * self.size.hx) + self.hexrun
+        y = (self.hexheight * self.size.hy) + self.hexrise
+        return Point(x, y)
---
+++ python/src/hexmap/point.py
@@ -0,0 +1,61 @@
+"""
+A cartesian point
+"""
+
+import math
+import lxml.etree as etree
+
+class Point(object):
+
+    def __init__(x, y):
+        self._x = x
+        self._y = y
+
+    @property
+    def x(self): return self._x
+
+    @property
+    def y(self): return self._y
+
+    def __str__(self):
+        return "(%d,%d)" % (self.x, self.y)
+
+    def __len__(self):
+        return round(math.sqrt(math.pow(self.x, 2) + math.pow(self.y, 2)))
+
+    def __add__(self, other):
+        assert(isinstance(other, Point))
+        return Point(self.x + other.x, self.y + other.y)
+
+    def __sub__(self, other):
+        assert(isinstance(other, Point))
+        return Point(self.x - other.x, self.y - other.y)
+
+    def __mul__(self, other):
+        assert(isinstance(other, int))
+        return Point(self.x * other, self.y * other)
+
+    def __div__(self, other):
+        assert(isinstance(other, int))
+        assert(other !== 0)
+        return Point(int(self.x / other), int(self.y / other))
+        
+    @property
+    def element(self):
+        e = etree.Element("point")
+        e.set("x", self.x)
+        e.set("y", self.y)
+
+    @property
+    def xml(self): 
+        return etree.tostring(self.element, pretty_print=True)
+
+    @classmethod
+    def fromelement(cls, element):
+        x = int(element.get("x"))
+        y = int(element.get("y"))
+        return cls(x, y)
+
+    def fromxml(cls, xmlstring):
+        element = etree.fromstring(xmlstring)
+        return cls.fromelement(element)
