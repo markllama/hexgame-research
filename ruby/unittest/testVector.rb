7b0d338aba61b0f964d84fbd8212175092f5b8ed
Mark Lamourine <markllama@gmail.com>
2011-01-29 21:11:13 -0500

    almost all of the tests complete for Vector

--- ruby/unittest/testVector.rb
+++ ruby/unittest/testVector.rb
@@ -85,7 +85,70 @@ class TestVector < Test::Unit::TestCase
   def testEqual
     hv0 = Vector.new(5, -6)
     hv1 = Vector.new(5, -6)
+    hv2 = Vector.new(4, 3)
 
-    assertEqual(hv0, hv1)
+    assert_equal(hv0, hv1)
+    assert_not_equal(hv0, hv2)
   end
+
+  def testLength
+    assert_equal(0, Vector.ORIGIN.length)
+    for i in 0..5
+      assert_equal(1, Vector.UNIT[i].length)
+    end
+
+    hv0 = Vector.new(4, 9)
+    assert_equal(9, hv0.length)
+
+    hv1 = Vector.new(-5, -10)
+    assert_equal(10, hv1.length)
+  end
+
+  def testDistance
+    assert_equal(0, Vector.ORIGIN.distance(Vector.ORIGIN))
+   
+    for i in 0..5
+      assert_equal(0, Vector.UNIT[i].distance(Vector.UNIT[i]))      
+    end
+
+    for i in 0..5
+      assert_equal(1, Vector.UNIT[i].distance(Vector.ORIGIN))
+      assert_equal(2, Vector.UNIT[i].distance(Vector.UNIT[(i + 3) % 6]))
+      assert_equal(1, Vector.ORIGIN.distance(Vector.UNIT[i]))
+    end
+
+    hv0 = Vector.new(4, 6)
+    hv1 = Vector.new(-10, -2)
+
+    assert_equal(14, hv0.distance(hv1))
+    assert_equal(14, hv1.distance(hv0))
+  end
+
+  def testHextant
+
+    # test on-axis
+
+    # test off-axis
+  end
+
+  def testRotate
+    
+    hv0 = Vector.UNIT[0]
+    for i in 0..5
+      assert_equal(Vector.UNIT[i], hv0.rotate(i))
+    end
+
+    for i in -6..-1
+      assert_equal(Vector.UNIT[i+6], hv0.rotate(i))
+    end
+
+    hv0 = Vector.new(0, 5)
+  end
+
+  def testBearing
+  end
+
+  def testAngle
+  end
+
 end
