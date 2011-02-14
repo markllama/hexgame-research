228264b8ad0d391aec4cdd3c75051c3154ca6187
Mark Lamourine <markllama@gmail.com>
2011-02-13 21:05:16 -0500

    added terrain tag to border terrain I really should add a set of ones for the class

--- python/src/hexmapview/borderterrain.py
+++ python/src/hexmapview/borderterrain.py
@@ -14,5 +14,5 @@ class BorderTerrain(Terrain):
             coordinates, 
             fill="white", 
             outline="black", 
-            tag=[self.name, "(%d,%d)" % (loc.hx, loc.hy)]
+            tag=[self.name, "terrain", "(%d,%d)" % (loc.hx, loc.hy)]
             )
