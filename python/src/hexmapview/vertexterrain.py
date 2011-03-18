e895cb281fca3befe275009d7040ff1cf019fc29
Mark Lamourine <markllama@gmail.com>
2011-03-17 20:43:16 -0400

    added more standard terrains

---
+++ python/src/hexmapview/vertexterrain.py
@@ -0,0 +1,18 @@
+from terrain import Terrain
+
+class Vertex(Terrain):
+    """Draw at the corner of a SuperBorder hex"""
+
+    def __init__(self, name, locations=None, map=None, color="black"):
+        Terrain.__init__(self, name, locations, map)
+        self._color = color
+
+    def repaint_location(self, loc):
+        center = self._map.hexcenter(loc)
+        vertices = [center.x - 4, center.y - 4, center.x - 4, center.y + 4]
+        self._map.create_rectangle(
+            vertices, 
+            fill=self._color,
+            tag = [self.name, "terrain", "(%d,%d)" % (loc.hx, loc.hy)]
+            )
+                                   
