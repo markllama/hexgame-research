326136cf77fe57efac89d556da68fbf70b19f431
Mark Lamourine <markllama@gmail.com>
2011-10-07 01:18:14 +0000

    completed tests for Iterator

--- javascript/unittest/testIterator.js
+++ javascript/unittest/testIterator.js
@@ -10,7 +10,7 @@ function Iterable(array) {
 };
 
 Iterable.prototype.first = function() {
-    if self.array.length = 0 {
+    if (self.array.length == 0) {
         return null;
     }
     self.current = 0;
@@ -44,4 +44,35 @@ function testFirst() {
     a0 = new Iterable([]);
     i0 = new Iterator(a0);
     assertEquals(null, i0.current());
+
+    a1 = new Iterable([0, 1, 2, 3, 4, 5]);
+    i1 = new Iterator(a1);
+    assertEquals(0, i1.current());
+};
+
+function testNext() {
+    a1 = new Iterable([0, 1, 2, 3, 4, 5]);
+    i1 = new Iterator(a1);
+    
+    assertEquals(0, i1.next());
+    assertEquals(1, i1.next());
+    assertEquals(2, i1.next());
+    assertEquals(3, i1.next());
+    assertEquals(4, i1.next());
+    assertEquals(5, i1.next());
+    assertEquals(null, i1.next());
+    assertEquals(null, i1.next());
+};
+
+function testReset() {
+    a1 = new Iterable([0, 1, 2, 3, 4, 5]);
+    i1 = new Iterator(a1);
+
+    assertEquals(0, i1.next());
+    assertEquals(1, i1.next());
+    assertEquals(2, i1.next());
+    assertEquals(3, i1.next());
+
+    i1.reset();
+    assertEquals(0, i1.next());
 };
