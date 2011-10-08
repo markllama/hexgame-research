e9ef922eaab66bacf2a577b6919c71eb99cff722
Mark Lamourine <markllama@gmail.com>
2011-10-08 02:45:03 +0000

    added hexmap and vector test templates

---
+++ javascript/unittest/testHexMap.js
@@ -0,0 +1,12 @@
+/*
+ *
+ * Test the point object
+ *
+ */
+
+function testHexMap() {
+
+    size = new HexMap.Vector(14,22);
+    origin = new HexMap.Vector(0, 0);
+};
+
---
+++ javascript/unittest/testHexMapVector.js
@@ -0,0 +1,12 @@
+/*
+ *
+ * Test the point object
+ *
+ */
+
+function testHexMapVector() {
+
+    size = new HexMap.Vector(14,22);
+    origin = new HexMap.Vector(0, 0);
+};
+
