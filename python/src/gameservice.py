cd7867d2cf63fff84c60eb3041cf79330b4e483f
Mark Lamourine <markllama@gmail.com>
2011-10-28 12:20:51 -0400

    start building game service

---
+++ python/src/gameservice.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+#
+# A game server
+#
+# Provides a RESTful interface to play hex games
+#
+
+import web
+import json
+import lxml
+import mimerender
+
+if __name__ == "__main__":
+    # become a daemon
+    print "hello"
