1e0f025d7ab6330320ab1cfd6dafd9e9423f749f
Mark Lamourine <markllama@gmail.com>
2012-02-26 15:20:20 -0500

    starting over

--- rails/hexgame/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Hexgame::Application.config.secret_token = '546498f4613f1ba74456947c0495f86918cb0c43dfe43fa146929b997e128127c659d0441b4e87bd4cda953e7c2e13204b0c3b059d287f9d69268e6515a0081b'
