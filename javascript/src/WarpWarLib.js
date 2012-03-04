0afbff0304217620058a756db303aeb8fac6e0fa
Mark Lamourine <markllama@gmail.com>
2012-03-04 18:51:57 -0500

    added warp lines

--- javascript/src/WarpWarLib.js
+++ javascript/src/WarpWarLib.js
@@ -104,3 +104,17 @@ WarpWarMapView.Terrain.WarpLine = function() {
 
 WarpWarMapView.Terrain.WarpLine.prototype = new WarpWarMapView.Terrain();
 
+WarpWarMapView.Terrain.WarpLine.prototype.draw = function() {
+    var ctx = this.map.canvas.getContext("2d");
+    var c0 = this.map.getHex(this.locations[0]).center();
+    var c1 = this.map.getHex(this.locations[1]).center();
+
+    ctx.fillStyle = "black";
+    ctx.lineWidth = 2;
+
+    ctx.beginPath();
+    ctx.moveTo(c0.x, c0.y);
+    ctx.lineTo(c1.x, c1.y);
+    ctx.closePath();
+    ctx.stroke();    
+};
